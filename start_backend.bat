16304235718d597d7a5881213850bc33a9f3c35c
Manira Mahat <rujesha11@gmail.com>
2025-12-26 11:29:09 +0545

    Remove obsolete documentation and scripts for backend and Flutter setup

--- start_backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\Users\user\Desktop\Agrinova\agri_python"
-echo Starting Django Backend Server...
-echo Keep this window open while using the Flutter app!
-echo.
-python manage.py runserver
-pause
